e2ddbadc75ee478d322cbb4011d306e949e7450c
Javier <jas77@alu.ua.es>
2025-02-22 12:07:51 +0100

    Migraciones completadas, tablas creadas, relaciones creadas

---
+++ tienda_camisetas/database/migrations/2025_02_22_102335_crear-tabla-equipo.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipo', function (Blueprint $table) {
+            $table->id()->primary(); // Esta ID es autoincremental
+            $table->string('nombre', 50);
+            $table->string('pais', 50);
+            $table->timestamps(); // Puedes quitar esta línea si no necesitas los timestamps
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipo');
+    }
+};
