3b5c0525e151d075808a3e1028bfe16d5989656f
Rm109-ua <Rm109@alu.ua.es>
2025-04-04 18:59:06 +0200

    login quitado

--- tienda_camisetas/resources/views/inicio.blade.php
+++ tienda_camisetas/resources/views/inicio.blade.php
@@ -15,7 +15,7 @@
     <!-- Formulario -->
     <div class="md:w-full w-[80%] max-w-lg mb-[190px] md:mb-0 bg-white p-10 rounded-[20px] shadow-lg border-t-[5px] border-t-orange-500 mt-6">
         <h1 class="text-4xl font-bold text-center mb-6">INICIO</h1>
-            <a href="/login" class="block text-center w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-500 hover:scale-105 duration-75 ease-in-out font-bold transition p-7">Login</a>
+            <!--<a href="/login" class="block text-center w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-500 hover:scale-105 duration-75 ease-in-out font-bold transition p-7">Login</a>-->
             <a href="/catalogo" class="block text-center w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-500 hover:scale-105 duration-75 ease-in-out font-bold transition p-7">Catálogo</a>
             <a href="/administrador" class="block text-center w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-500 hover:scale-105 duration-75 ease-in-out font-bold transition p-7">Administrador</a>
     </div>
