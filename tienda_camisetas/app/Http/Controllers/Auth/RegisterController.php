206a0a0980c8f656eab14db5d3ebb3a85846f127
Rm109-ua <Rm109@alu.ua.es>
2025-05-19 12:24:41 +0200

    registro funcionando!

--- tienda_camisetas/app/Http/Controllers/Auth/RegisterController.php
+++ tienda_camisetas/app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'dni' => ['required', 'string', 'max:255'],
-            'nombre' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
